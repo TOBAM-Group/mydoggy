e8b1a68fe55059d4821d4a4269dcf722d2441138
adecaro <adecaro@2a62bbde-051e-0410-bf3e-cfb6b34e140d>
2006-10-24 12:04:21 +0000

    Pre release changes.
    git-svn-id: https://svn.code.sf.net/p/mydoggy/code/mydoggy/trunk@14 2a62bbde-051e-0410-bf3e-cfb6b34e140d

---
+++ mydoggy-examples/src/script/mydoggyset.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# ------------------------------------------------------
+#  MyDoggySet Startup Script for Unix
+# ------------------------------------------------------
+#
+
+# ---------------------------------------------------------------------
+# Before you run IntelliJ IDEA specify the location of the
+# JDK 1.5 or higher installation directory which will be used for running IDEA
+# ---------------------------------------------------------------------
+if [ -z "$MYDOGGY_JDK" ]; then
+  MYDOGGY_JDK=$JAVA_HOME
+  if [ -z "MYDOGGY_JDK" ]; then
+    echo ERROR: cannot start MyDoggySet.
+    echo No JDK found to run. Please validate either MYDOGGY_JDK or JAVA_HOME
+    echo points to valid installation path.
+  fi
+fi
+
+MYDOGGY_HOME=`dirname "$0"`/..
+MYDOGGY_BIN_HOME=`dirname "$0"`
+
+export JAVA_HOME
+export MYDOGGY_HOME
+
+MYDOGGY_MCN="org.noos.xing.mydoggy.examples.mydoggyset.MyDoggySet"
+
+CLASSPATH=../lib/mydoggy-api-1.0.0.jar
+CLASSPATH=$CLASSPATH:../lib/mydoggy-plaf-1.0.0.jar
+CLASSPATH=$CLASSPATH:../lib/mydoggy-examples-1.0.0.jar
+CLASSPATH=$CLASSPATH:../lib/TableLayout-20020517.jar
+
+export CLASSPATH
+
+LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+
+cd "$MYDOGGY_BIN_HOME"
+exec $MYDOGGY_JDK/bin/java $JVM_ARGS $MYDOGGY_MCN $*
