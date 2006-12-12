2f67d6278687c6ccbcbfc542e444a89784d5f54b
adecaro <adecaro@2a62bbde-051e-0410-bf3e-cfb6b34e140d>
2006-12-12 12:46:54 +0000

    Some updates.
    git-svn-id: https://svn.code.sf.net/p/mydoggy/code/mydoggy/trunk@93 2a62bbde-051e-0410-bf3e-cfb6b34e140d

---
+++ mydoggy-plaf/src/main/java/org/noos/xing/mydoggy/plaf/ui/ToFrontWindowFocusListener.java
@@ -0,0 +1,32 @@
+package org.noos.xing.mydoggy.plaf.ui;
+
+import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+
+/**
+ * @author Angelo De Caro (angelo.decaro@gmail.com)
+ */
+public class ToFrontWindowFocusListener implements WindowFocusListener {
+    long start;
+    long end;
+    private final JDialog dialog;
+
+    public ToFrontWindowFocusListener(JDialog dialog) {
+        this.dialog = dialog;
+    }
+
+    public void windowGainedFocus(WindowEvent e) {
+        start = System.currentTimeMillis();
+    }
+
+    public void windowLostFocus(WindowEvent e) {
+        end = System.currentTimeMillis();
+        long elapsed = end - start;
+        //System.out.println(elapsed);
+        if (elapsed < 100)
+            dialog.toFront();
+
+        dialog.removeWindowFocusListener(this);
+    }
+}
