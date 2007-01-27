99c29d60cd3ca63b903250bb5f4b511bca7b0263
adecaro <adecaro@2a62bbde-051e-0410-bf3e-cfb6b34e140d>
2007-01-27 19:09:25 +0000

    New on Persistence Machanism...
    git-svn-id: https://svn.code.sf.net/p/mydoggy/code/mydoggy/trunk@164 2a62bbde-051e-0410-bf3e-cfb6b34e140d

---
+++ mydoggy-plaf/src/main/java/org/noos/xing/mydoggy/plaf/persistence/PersistedSlidingType.java
@@ -0,0 +1,37 @@
+package org.noos.xing.mydoggy.plaf.persistence;
+
+import org.xml.sax.Attributes;
+
+/**
+ * @author Angelo De Caro (angelo.decaro@gmail.com)
+ */
+public class PersistedSlidingType {
+    private boolean transparentMode;
+    private float transparentRatio;
+    private int transparentDelay;
+    private boolean enabled;
+
+    public PersistedSlidingType(Attributes attributes) {
+        this.transparentMode = Boolean.parseBoolean(attributes.getValue("transparentMode"));
+        this.transparentDelay = Integer.parseInt(attributes.getValue("transparentDelay"));
+        this.transparentRatio = Float.parseFloat(attributes.getValue("transparentRatio"));
+        this.enabled = Boolean.parseBoolean(attributes.getValue("enabled"));
+    }
+
+
+    public boolean isTransparentMode() {
+        return transparentMode;
+    }
+
+    public float getTransparentRatio() {
+        return transparentRatio;
+    }
+
+    public int getTransparentDelay() {
+        return transparentDelay;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
