83e2ccb80183eb9e86a2681c98f0ecfdbcfaca38
adecaro <adecaro@2a62bbde-051e-0410-bf3e-cfb6b34e140d>
2007-01-27 19:16:51 +0000

    New on Persistence Machanism...
    git-svn-id: https://svn.code.sf.net/p/mydoggy/code/mydoggy/trunk@169 2a62bbde-051e-0410-bf3e-cfb6b34e140d

---
+++ mydoggy-plaf/src/main/java/org/noos/xing/mydoggy/plaf/persistence/PersistedFloatingType.java
@@ -0,0 +1,74 @@
+package org.noos.xing.mydoggy.plaf.persistence;
+
+import org.xml.sax.Attributes;
+
+import java.awt.*;
+
+/**
+ * @author Angelo De Caro (angelo.decaro@gmail.com)
+ */
+public class PersistedFloatingType {
+    private boolean transparentMode;
+    private float transparentRatio;
+    private int transparentDelay;
+    private boolean enabled;
+    private boolean modal;
+    private Point location;
+    private Dimension size;
+
+    public PersistedFloatingType(Attributes attributes) {
+        this.transparentMode = Boolean.parseBoolean(attributes.getValue("transparentMode"));
+        this.transparentDelay = Integer.parseInt(attributes.getValue("transparentDelay"));
+        this.transparentRatio = Float.parseFloat(attributes.getValue("transparentRatio"));
+        this.enabled = Boolean.parseBoolean(attributes.getValue("enabled"));
+        this.modal = Boolean.parseBoolean(attributes.getValue("modal"));
+
+        if (attributes.getValue("x") != null)
+            this.location = new Point(
+                    Integer.parseInt(attributes.getValue("x")),
+                    Integer.parseInt(attributes.getValue("y"))
+            );
+
+        if (attributes.getValue("width") != null)  
+            this.size = new Dimension(
+                    Integer.parseInt(attributes.getValue("width")),
+                    Integer.parseInt(attributes.getValue("height"))
+            );
+    }
+
+    public boolean isTransparentMode() {
+        return transparentMode;
+    }
+
+    public float getTransparentRatio() {
+        return transparentRatio;
+    }
+
+    public int getTransparentDelay() {
+        return transparentDelay;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public boolean isModal() {
+        return modal;
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+
+    public Dimension getSize() {
+        return size;
+    }
+
+    public void setSize(Dimension size) {
+        this.size = size;
+    }
+}
---
+++ mydoggy-plaf/src/main/java/org/noos/xing/mydoggy/plaf/persistence/xml/MyDoggyHandler.java
@@ -0,0 +1,157 @@
+package org.noos.xing.mydoggy.plaf.persistence.xml;
+
+import org.noos.xing.mydoggy.*;
+import org.noos.xing.mydoggy.plaf.persistence.PersistedDockedType;
+import org.noos.xing.mydoggy.plaf.persistence.PersistedFloatingType;
+import org.noos.xing.mydoggy.plaf.persistence.PersistedToolWindow;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.awt.*;
+
+/**
+ * @author Angelo De Caro (angelo.decaro@gmail.com)
+ */
+public class MyDoggyHandler extends DefaultHandler {
+    enum State {
+        NOP, MYDOGGY, TOOLS, TOOL, DESCRIPTOR
+    }
+
+    private ToolWindowManager toolWindowManager;
+
+    private State state;
+    private State subState;
+    private PersistedToolWindow persistedToolWindow;
+    private PersistedDockedType dockedType;
+    private PersistedSlidingType slidingType;
+    private PersistedFloatingType floatingType;
+
+
+    public MyDoggyHandler(ToolWindowManager toolWindowManager) {
+        this.toolWindowManager = toolWindowManager;
+    }
+
+    public void startDocument() throws SAXException {
+        state = State.MYDOGGY;
+        subState = State.NOP;
+    }
+
+    public void endDocument() throws SAXException {
+    }
+
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        // TODO: check for errors...
+        switch (state) {
+            case MYDOGGY:
+                if ("mydoggy".equals(qName)) {
+                    // Check version
+                    if (attributes.getLength() <= 0)
+                        throw new SAXException("Invalid version. Not defined.");
+
+                    String version = attributes.getValue(0);
+                    if (!"1.2.0".equals(version))
+                        throw new SAXException("Invalid version. " + version);
+
+                    if (attributes.getValue("pushAwayMode") != null)
+                        toolWindowManager.getToolWindowManagerDescriptor().setPushAwayMode(
+                                ToolWindowManagerDescriptor.PushAwayMode.valueOf(attributes.getValue("pushAwayMode"))
+                        );
+
+                    state = State.TOOLS;
+                } else
+                    throw new SAXException("Invalid element at this position. Aspeting <mydoggy>");
+                break;
+            case TOOLS:
+                if (!"tools".equals(qName))
+                    throw new SAXException("Invalid element at this position. Aspeting <tools>");
+                state = State.TOOL;
+                break;
+            case TOOL:
+                if (subState == State.NOP) {
+                    if (!"tool".equals(qName))
+                        throw new SAXException("Invalid element at this position. Aspeting <tool>");
+
+                    persistedToolWindow = new PersistedToolWindow(attributes);
+                    dockedType = null;
+                    slidingType = null;
+                    floatingType = null;
+                    subState = State.DESCRIPTOR;
+                } else {
+                    if ("docked".equals(qName)) {
+                        dockedType = new PersistedDockedType(attributes);
+                    } else if ("sliding".equals(qName)) {
+                        slidingType = new PersistedSlidingType(attributes);
+                    } else if ("floating".equals(qName)) {
+                        floatingType = new PersistedFloatingType(attributes);
+                    }
+                }
+                break;
+        }
+    }
+
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        switch (state) {
+            case TOOL:
+                if ("tool".equals(qName)) {
+                    subState = State.NOP;
+
+                    ToolWindow toolWindow = toolWindowManager.getToolWindow(persistedToolWindow.getId());
+
+                    // Apply descriptors
+                    if (dockedType != null) {
+                        DockedTypeDescriptor descriptor = (DockedTypeDescriptor) toolWindow.getTypeDescriptor(ToolWindowType.DOCKED);
+                        descriptor.setDockLength(dockedType.getDockLength());
+                        descriptor.setPopupMenuEnabled(dockedType.isPopupMenuEnabled());
+                    }
+
+                    if (slidingType != null) {
+                        SlidingTypeDescriptor descriptor = (SlidingTypeDescriptor) toolWindow.getTypeDescriptor(ToolWindowType.SLIDING);
+                        descriptor.setEnabled(slidingType.isEnabled());
+                        descriptor.setTransparentDelay(slidingType.getTransparentDelay());
+                        descriptor.setTransparentMode(slidingType.isTransparentMode());
+                        descriptor.setTransparentRatio(slidingType.getTransparentRatio());
+                    }
+
+                    if (floatingType != null) {
+                        FloatingTypeDescriptor descriptor = (FloatingTypeDescriptor) toolWindow.getTypeDescriptor(ToolWindowType.FLOATING);
+                        descriptor.setEnabled(floatingType.isEnabled());
+                        descriptor.setTransparentDelay(floatingType.getTransparentDelay());
+                        descriptor.setTransparentMode(floatingType.isTransparentMode());
+                        descriptor.setTransparentRatio(floatingType.getTransparentRatio());
+                        descriptor.setModal(floatingType.isModal());
+
+                        Point point = floatingType.getLocation();
+                        if (point != null)
+                            descriptor.setLocation(point.x, point.y);
+                        Dimension dimension = floatingType.getSize();
+                        if (dimension != null)
+                            descriptor.setSize(dimension.width, dimension.height);
+                    }
+
+                    toolWindow.setAnchor(persistedToolWindow.getAnchor());
+                    toolWindow.setType(persistedToolWindow.getType());
+                    toolWindow.setAutoHide(persistedToolWindow.isAutoHide());
+                    toolWindow.setAvailable(persistedToolWindow.isAvailable());
+
+                    if (toolWindow.getType() == ToolWindowType.FLOATING || toolWindow.getType() == ToolWindowType.FLOATING_FREE) {
+                        toolWindow.setVisible(persistedToolWindow.isVisible());
+                    } else {
+                        if (persistedToolWindow.isVisible())
+                            toolWindow.aggregate();
+                        else
+                            toolWindow.setVisible(false);
+                    }
+                    toolWindow.setActive(persistedToolWindow.isActive());
+                }
+                break;
+            case TOOLS:
+                state = State.TOOLS;
+                break;
+            case MYDOGGY:
+                state = State.MYDOGGY;
+                break;
+        }
+    }
+
+}
