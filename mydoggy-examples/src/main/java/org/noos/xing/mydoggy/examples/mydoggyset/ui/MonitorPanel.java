a56f90f7899f0aaa42f66e0855f30e5f2503c2e7
adecaro <adecaro@2a62bbde-051e-0410-bf3e-cfb6b34e140d>
2007-05-06 08:26:29 +0000

    Some updates...
    git-svn-id: https://svn.code.sf.net/p/mydoggy/code/mydoggy/trunk@293 2a62bbde-051e-0410-bf3e-cfb6b34e140d

--- mydoggy-examples/src/main/java/org/noos/xing/mydoggy/examples/mydoggyset/ui/MonitorPanel.java
+++ mydoggy-examples/src/main/java/org/noos/xing/mydoggy/examples/mydoggyset/ui/MonitorPanel.java
@@ -39,25 +39,28 @@ public class MonitorPanel extends JPanel {
     }
 
     protected class Surface extends JPanel implements Runnable {
-        private MonitorSource monitorSource;
         private Thread thread;
+        private MonitorSource monitorSource;
 
         private long sleepAmount = 100;
+
         private int w, h;
-        private BufferedImage bimg;
-        private Graphics2D big;
+        private BufferedImage backImage;
+        private Graphics2D backImageGrfx;
+
         private Font font = new Font("Times New Roman", Font.PLAIN, 11);
         private int columnInc;
-        private double[] pts;
-        private int ptNum;
+
+        private double[] points;
+        private int validPoints;
+
         private int ascent, descent;
-        private Rectangle graphOutlineRect = new Rectangle();
+
         private Rectangle2D mfRect = new Rectangle2D.Float();
         private Rectangle2D muRect = new Rectangle2D.Float();
         private Line2D graphLine = new Line2D.Float();
         private Color graphColor = new Color(46, 139, 87);
         private Color mfColor = new Color(0, 100, 0);
-        private String usedStr;
 
         public Surface(MonitorSource monitorSource) {
             this.monitorSource = monitorSource;
@@ -66,29 +69,32 @@ public class MonitorPanel extends JPanel {
 
         public void paint(Graphics g) {
             Dimension d = getSize();
+
             if (d.width != w || d.height != h) {
                 w = d.width;
                 h = d.height;
-                bimg = (BufferedImage) createImage(w, h);
-                big = bimg.createGraphics();
-                big.setFont(font);
-                FontMetrics fm = big.getFontMetrics(font);
+
+                backImage = (BufferedImage) createImage(w, h);
+                backImageGrfx = backImage.createGraphics();
+                backImageGrfx.setFont(font);
+
+                FontMetrics fm = backImageGrfx.getFontMetrics(font);
                 ascent = fm.getAscent();
                 descent = fm.getDescent();
             }
 
-            big.setBackground(getBackground());
-            big.clearRect(0, 0, w, h);
+            backImageGrfx.setBackground(getBackground());
+            backImageGrfx.clearRect(0, 0, w, h);
 
             float totalMemory = monitorSource.getTotal();
             float usedMemory = monitorSource.getUsed();
             float freeMemory = totalMemory - usedMemory;
 
             // .. Draw allocated and used strings ..
-            big.setColor(Color.green);
-            big.drawString(String.valueOf((int) totalMemory >> 10) + "K allocated", 4.0f, (float) ascent + 0.5f);
-            usedStr = String.valueOf(((int) usedMemory) >> 10) + "K used";
-            big.drawString(usedStr, 4, h - descent);
+            backImageGrfx.setColor(Color.green);
+            backImageGrfx.drawString(String.valueOf((int) totalMemory >> 10) + "K allocated", 4.0f, (float) ascent + 0.5f);
+            String usedStr = String.valueOf(((int) usedMemory) >> 10) + "K used";
+            backImageGrfx.drawString(usedStr, 4, h - descent);
 
             // Calculate remaining size
             float ssH = ascent + descent;
@@ -98,23 +104,23 @@ public class MonitorPanel extends JPanel {
             float remainingWidth = (w - blockWidth - 10);
 
             // .. Memory Free ..
-            big.setColor(mfColor);
+            backImageGrfx.setColor(mfColor);
             int MemUsage = (int) (freeMemory / totalMemory * 10);
             int i = 0;
             for (; i < MemUsage; i++) {
                 mfRect.setRect(5, ssH + i * blockHeight, blockWidth, blockHeight - 1);
-                big.fill(mfRect);
+                backImageGrfx.fill(mfRect);
             }
 
             // .. Memory Used ..
-            big.setColor(Color.green);
+            backImageGrfx.setColor(Color.green);
             for (; i < 10; i++) {
                 muRect.setRect(5, ssH + i * blockHeight, blockWidth, blockHeight - 1);
-                big.fill(muRect);
+                backImageGrfx.fill(muRect);
             }
 
             // .. Draw History Graph ..
-            big.setColor(graphColor);
+            backImageGrfx.setColor(graphColor);
             int graphX = 30;
             int graphY = (int) ssH;
             int graphW = w - graphX - 5;
@@ -123,7 +129,7 @@ public class MonitorPanel extends JPanel {
             i = 0;
             for (; i < 10; i++) {
                 muRect.setRect(graphX, ssH + i * blockHeight - 1, graphW, blockHeight);
-                big.draw(muRect);
+                backImageGrfx.draw(muRect);
             }
 
             // .. Draw animated column movement ..
@@ -135,52 +141,43 @@ public class MonitorPanel extends JPanel {
 
             for (int j = graphX + columnInc; j < graphW + graphX; j += graphColumn) {
                 graphLine.setLine(j, graphY, j, ssH + i * blockHeight - 1);
-                big.draw(graphLine);
+                backImageGrfx.draw(graphLine);
             }
 
             --columnInc;
 
-            if (pts == null) {
-                pts = new double[graphW];
-                ptNum = 0;
-            } else if (pts.length != graphW) {
+            if (points == null) {
+                points = new double[graphW];
+                validPoints = 0;
+            } else if (points.length != graphW) {
                 double[] tmp;
-                if (ptNum < graphW) {
-                    tmp = new double[ptNum];
-                    System.arraycopy(pts, 0, tmp, 0, tmp.length);
+                if (validPoints < graphW) {
+                    tmp = new double[validPoints];
+                    System.arraycopy(points, 0, tmp, 0, tmp.length);
                 } else {
                     tmp = new double[graphW];
-                    System.arraycopy(pts, pts.length - tmp.length, tmp, 0, tmp.length);
-                    ptNum = tmp.length - 2;
+                    System.arraycopy(points, points.length - tmp.length, tmp, 0, tmp.length);
+                    validPoints = tmp.length - 2;
                 }
-                pts = new double[graphW];
-                System.arraycopy(tmp, 0, pts, 0, tmp.length);
+                points = new double[graphW];
+                System.arraycopy(tmp, 0, points, 0, tmp.length);
             } else {
-                big.setColor(Color.yellow);
+                backImageGrfx.setColor(Color.yellow);
+                int x = w - 5;
                 int sum = graphY + graphH;
 
-//                pts[ptNum] = (int) (graphY + graphH * (freeMemory / totalMemory));
-                for (int j = sum - ptNum, k = 0; k < ptNum; k++, j++) {
+                for (int j = x - validPoints, k = 0; k < validPoints; k++, j++) {
                     if (k != 0) {
-                        if (pts[k] != pts[k - 1]) {
-                            big.drawLine(j - 1, (int) (sum * pts[k - 1]), j, (int) (sum * pts[k]));
+                        if (points[k] != points[k - 1]) {
+                            backImageGrfx.drawLine(j - 1, (int) (sum * points[k - 1]), j, (int) (sum * points[k]));
                         } else {
-                            big.fillRect(j, (int) (sum * pts[k]), 1, 1);
+                            backImageGrfx.fillRect(j, (int) (sum * points[k]), 1, 1);
                         }
                     }
                 }
-/*
-                if (ptNum + 2 == pts.length) {
-                    // throw out oldest point
-                    System.arraycopy(pts, 1, pts, 0, ptNum);
-                    --ptNum;
-                } else {
-                    ptNum++;
-                }
-*/
             }
 
-            g.drawImage(bimg, 0, 0, this);
+            g.drawImage(backImage, 0, 0, this);
         }
 
         public void run() {
@@ -190,35 +187,34 @@ public class MonitorPanel extends JPanel {
                 float usedMemory = monitorSource.getUsed();
                 float freeMemory = totalMemory - usedMemory;
 
-                if (pts == null) {
-                    pts = new double[1];
-                    ptNum = 0;
-                } else if (pts.length < ptNum + 1) {
+                if (points == null) {
+                    points = new double[1];
+                    validPoints = 0;
+                } else if (points.length < validPoints + 1) {
                     double[] tmp;
-                    int graphW = ptNum+1;
-                    if (ptNum < graphW) {
-                        tmp = new double[ptNum];
-                        System.arraycopy(pts, 0, tmp, 0, tmp.length);
+                    int graphW = validPoints +1;
+                    if (validPoints < graphW) {
+                        tmp = new double[validPoints];
+                        System.arraycopy(points, 0, tmp, 0, tmp.length);
                     } else {
                         tmp = new double[graphW];
-                        System.arraycopy(pts, pts.length - tmp.length, tmp, 0, tmp.length);
-                        ptNum = tmp.length - 2;
+                        System.arraycopy(points, points.length - tmp.length, tmp, 0, tmp.length);
+                        validPoints = tmp.length - 2;
                     }
-                    pts = new double[graphW];
-                    System.arraycopy(tmp, 0, pts, 0, tmp.length);
+                    points = new double[graphW];
+                    System.arraycopy(tmp, 0, points, 0, tmp.length);
                 } else {
-                    pts[ptNum] = (freeMemory / totalMemory);
-                    if (ptNum + 2 == pts.length) {
+                    points[validPoints] = (freeMemory / totalMemory);
+                    if (validPoints + 2 == points.length) {
                         // throw out oldest point
-                        System.arraycopy(pts, 1, pts, 0, ptNum);
-                        --ptNum;
+                        System.arraycopy(points, 1, points, 0, validPoints);
+                        --validPoints;
                     } else {
-                        ptNum++;
+                        validPoints++;
                     }
                 }
 
-//                    if (isShowing())
-                    repaint();
+                repaint();
 
                 try {
                     Thread.sleep(sleepAmount);
