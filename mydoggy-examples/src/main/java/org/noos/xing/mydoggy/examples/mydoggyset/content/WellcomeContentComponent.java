a858b313e8d50977c27f28485742c5465dddcc59
adecaro <adecaro@2a62bbde-051e-0410-bf3e-cfb6b34e140d>
2007-08-11 14:20:45 +0000

    mydoggyset: interactive tests, some updates...
    git-svn-id: https://svn.code.sf.net/p/mydoggy/code/mydoggy/trunk@514 2a62bbde-051e-0410-bf3e-cfb6b34e140d

--- mydoggy-examples/src/main/java/org/noos/xing/mydoggy/examples/mydoggyset/content/WellcomeContentComponent.java
+++ mydoggy-examples/src/main/java/org/noos/xing/mydoggy/examples/mydoggyset/content/WellcomeContentComponent.java
@@ -3,8 +3,10 @@ package org.noos.xing.mydoggy.examples.mydoggyset.content;
 import info.clearthought.layout.TableLayout;
 import org.noos.xing.mydoggy.ToolWindowManager;
 import org.noos.xing.mydoggy.plaf.ui.util.Colors;
+import org.noos.xing.mydoggy.plaf.ui.border.LineBorder;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author Angelo De Caro (angelo.decaro@gmail.com)
@@ -20,15 +22,50 @@ public class WellcomeContentComponent extends JPanel {
     protected void initComponents() {
 
         // Setup wellcome panel...
-        JPanel panel = new JPanel(new TableLayout(new double[][]{{-1},{-1}}));
+        JPanel panel = new JPanel(new TableLayout(new double[][]{{-1, 100, 5, 160, 3, 100, 5, 160, -1}, 
+                                                                 {-1, 70, 10, 70, 10, 70, -1}}));
         panel.setBackground(Colors.blu);
 
+        JButton manager = new JButton("Manager");
+        manager.setOpaque(false);
+        manager.setContentAreaFilled(false);
+        manager.setFocusPainted(false);
+        manager.setBorder(new LineBorder(Color.WHITE));
+
+        JLabel managerLabel = new JLabel("<html>Content to setup all manager </br> properties </html>");
+        managerLabel.setForeground(Color.WHITE);
+
+        JButton tools = new JButton("tools");
+        JButton contents = new JButton("contents");
+        JButton groups = new JButton("groups");
+        JButton itests = new JButton("itests");
+
+        panel.add(manager, "1,1,FULL,FULL");
+        panel.add(managerLabel, "3,1,FULL,FULL");
+
+        panel.add(tools, "1,3,FULL,FULL");
+        panel.add(contents, "1,5,FULL,FULL");
+
+        panel.add(groups, "5,1,FULL,FULL");
+        panel.add(itests, "5,3,FULL,FULL");
+
         // Setup main panel
-        setLayout(new TableLayout(new double[][]{{-1},{93,5,-1}}));
+        setLayout(new TableLayout(new double[][]{{3, -1, 3},{3, 93,5,-1,3}}));
         add(new JLabel(new ImageIcon(
                 this.getClass().getClassLoader().getResource("org/noos/xing/mydoggy/examples/mydoggyset/images/banner.jpg")
-        )), "0,0,FULL,FULL");
-        add(panel, "0,2,FULL,FULL");
+        )), "1,1,FULL,FULL");
+        add(panel, "1,3,FULL,FULL");
+    }
+
+    protected void render(JPanel panel, Icon buttonIcon, String labelText) {
+        JButton manager = new JButton(buttonIcon);
+        manager.setOpaque(false);
+        manager.setContentAreaFilled(false);
+        manager.setFocusPainted(false);
+        manager.setBorder(new LineBorder(Color.WHITE));
+
+        JLabel managerLabel = new JLabel(labelText);
+        managerLabel.setForeground(Color.WHITE);
     }
 
 
