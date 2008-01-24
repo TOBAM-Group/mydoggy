df852c921bf45bb4cb32d3e7a8b3ead18e1064e8
adecaro <adecaro@2a62bbde-051e-0410-bf3e-cfb6b34e140d>
2008-01-24 18:52:07 +0000

    something new. setShowAlwayTab for MultiSplitContentUI and detached support...
    git-svn-id: https://svn.code.sf.net/p/mydoggy/code/mydoggy/trunk@870 2a62bbde-051e-0410-bf3e-cfb6b34e140d

---
+++ mydoggy-api/src/main/java/org/noos/xing/mydoggy/MultiSplitContentUI.java
@@ -0,0 +1,36 @@
+package org.noos.xing.mydoggy;
+
+/**
+ * This interface let you modify ui behaviours of a content when a <code>TabbedContentManagerUI</code> is used
+ * as current <code>ContentManagerUI</code>.
+ *
+ * @author Angelo De Caro (angelo.decaro@gmail.com)
+ * @see org.noos.xing.mydoggy.TabbedContentManagerUI
+ * @since 1.1.0
+ */
+public interface MultiSplitContentUI extends TabbedContentUI {
+
+    /**
+     * Sets whether or not the ui must show a tab for all contents also
+     * when there is just one content.
+     *
+     * @param showAlwaysTab
+     * @see #isShowAlwaysTab()
+     * @since 1.4.1
+     */
+    void setShowAlwaysTab(boolean showAlwaysTab);
+
+    /**
+     * Returns whether or not the ui must show a tab for all contents also
+     * when there is just one content.
+     * Default value is false.
+     *
+     * @return true if the the ui must show a tab for all contents;
+     *         false otherwise
+     * @see #setShowAlwaysTab(boolean)
+     * @since 1.4.1
+     */
+    boolean isShowAlwaysTab();
+
+
+}
